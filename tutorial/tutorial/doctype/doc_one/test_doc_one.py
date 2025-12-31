8f76bef4621acc6d78c7cb23fbcef558325d467e
sumandas-clapgrow <suman.das@clapgrow.com>
2025-12-31 13:18:20 +0530

    fsfdf

--- tutorial/tutorial/doctype/doc_one/test_doc_one.py
+++ tutorial/tutorial/doctype/doc_one/test_doc_one.py
@@ -1,9 +1,16 @@
 # Copyright (c) 2025, suman and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests.utils import FrappeTestCase
 
 
 class TestDocOne(FrappeTestCase):
-	pass
+	def test_full_name_correcty_set(self):
+		test_doc = frappe.new_doc("Doc One")
+		test_doc.name1 = "Test Doc One"
+		test_doc.last_name = "Test Doc One Title"
+		test_doc.phone = "+918587854125"
+		test_doc.save()
+
+		self.assertEqual(test_doc.name1,"Test Doc One")
